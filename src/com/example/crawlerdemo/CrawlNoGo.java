2fcdac3c90cf7186093ab85949b158c86e0cfa29
tjm5114 <tjm5114@gmail.com>
2014-04-07 17:20:44 -0400

    hang up on checkbox implementation
    trying to implement a checkbox to save which bars not to go

--- src/com/example/crawlerdemo/CrawlNoGo.java
+++ src/com/example/crawlerdemo/CrawlNoGo.java
@@ -9,10 +9,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.CheckBox;
 
 public class CrawlNoGo extends Activity {
-
+	
+	//create tour parameters array 
 	ArrayList<String> tourParms = new ArrayList<String>();
+	
+	//create checkbox variables
+	CheckBox checkboxAllenGrill = (CheckBox)findViewById(R.id.allenGrill);
+//	CheckBox checkboxBarBlue = (CheckBox) findViewById(R.id.barBleu);
+//	CheckBox checkboxBrewery = (CheckBox) findViewById(R.id.brewery);
+//	CheckBox checkboxCafe = (CheckBox) findViewById(R.id.cafe);
+//	CheckBox checkboxChilis = (CheckBox) findViewById(R.id.chilis);
+//	CheckBox checkboxChrome = (CheckBox) findViewById(R.id.chrome);
+//	CheckBox checkboxChumleys = (CheckBox) findViewById(R.id.chumleys);
+//	CheckBox checkboxDarkhorse = (CheckBox) findViewById(R.id.darkhorse);
+//	CheckBox checkboxGman = (CheckBox) findViewById(R.id.gman);
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +45,59 @@ public class CrawlNoGo extends Activity {
 		return true;
 	}
 	
-	public void CrawlNoGo(View v) {
+	public void newActivity(View v) {
 		Intent intent = new Intent(this, CrawlNoGo2.class);
  	    
 		//fire NoGo Bar names to array function
-		saveNoGoBars();
+		//saveNoGoBars();		
 		
 		//start new activity CrawlNoGo2
  	    startActivity(intent);
 		
 	}
 	
-	public void saveNoGoBars(){
-		//loop through each checkbox and check if the state is true then save it to tourParms array
-		
-		
-	}
+//	public void saveNoGoBars(){
+//		//loop through each checkbox and check if the state is true then save it to tourParms array
+//		if(checkboxAllenGrill.isActivated())
+//			{
+//				tourParms.add("allenGrill");
+//			}
+//		if(checkboxBarBlue.isActivated())
+//		{
+//			tourParms.add("barBleu");
+//		}
+//		if(checkboxBrewery.isActivated())
+//		{
+//			tourParms.add("brewery");
+//		}
+//		if(checkboxCafe.isActivated())
+//		{
+//			tourParms.add("cafe");
+//		}
+//		if(checkboxChilis.isActivated())
+//		{
+//			tourParms.add("chilis");
+//		}
+//		if(checkboxChrome.isActivated())
+//		{
+//			tourParms.add("chrome");
+//		}
+//		if(checkboxChumleys.isActivated())
+//		{
+//			tourParms.add("chumleys");
+//		}
+//		if(checkboxDarkhorse.isActivated())
+//		{
+//			tourParms.add("darkhorse");
+//		}
+//		if(checkboxGman.isActivated())
+//		{
+//			tourParms.add("gman");
+//		}
+//		
+//		System.out.println(tourParms);
+//		
+//		
+//	}
 
 }
